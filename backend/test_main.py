9ece0cb95ffbc899e7a2165dce3609856a50f7d9
Okeybukks <achebepeter94@gmail.com>
2023-10-30 11:44:43 +0100

    test

--- backend/test_main.py
+++ backend/test_main.py
@@ -14,9 +14,7 @@ class TestFlaskApp(unittest.TestCase):
 
     def test_proxy_route_content(self):
         response = self.app.get('/')
-        print(response)
         data = response.get_json()
-        print(data)
         self.assertTrue(data['success'])  # Check if 'success' is True
         self.assertEqual(len(data['result']), 2)  # Check if there are 2 results
 

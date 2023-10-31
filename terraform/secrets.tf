5e995e449556ea71f9f67f7cad7ecebbd90c0b9b
Sheriff <realexcel2020@gmail.com>
2023-10-31 02:32:55 +0100

    changed secrets name

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,13 +1,13 @@
 # API keys are stored as secrets and will be manally entered in the console
 
 resource "aws_secretsmanager_secret" "zone" {
-  name = "ZONE_ID-2"
+  name = "ZONE_ID-3"
 }
 
 resource "aws_secretsmanager_secret" "api_key" {
-  name = "CF_API_KEY-2"
+  name = "CF_API_KEY-3"
 }
 
 resource "aws_secretsmanager_secret" "api_email" {
-  name = "CF_API_EMAIL-2"
+  name = "CF_API_EMAIL-3"
 }

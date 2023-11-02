7272be72176fff95a7bcf09eafe2c8d3cbc7fe15
fajmayor <fajmayor@gmail.com>
2023-11-02 10:52:43 +0100

    added deploy aws infra step to pipeline

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,7 +1,6 @@
 # configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
   region = var.region
-  #profile = "terraform-user"
 
   default_tags {
     tags = {

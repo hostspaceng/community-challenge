ccf1f0f9872b67370e40b243fb831810e9351302
abdulfrfr <abdulmails112@gmail.com>
2023-11-03 00:30:05 +0100

    fixed CORS issues

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+ aws cloudformation create-stack   --region us-east-1   --stack-name my-eks-vpc   --template-body file://vpc_stack.yaml 
+ aws cloudformation create-stack   --region us-east-1   --stack-name my-eks-cluster   --capabilities CAPABILITY_NAMED_IAM   --template-body file://hostober_challenge_cloudformation.yaml
+
 aws eks update-kubeconfig --name my-eks-cluster --region us-east-1
 
 
 helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx
 
-kubectl apply -f eks_configmap.yaml
 
 kubectl apply -f eks_deployments.yaml
 

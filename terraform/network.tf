d4ea6035c7e6500ce23fb3678e7592f7456ecddd
Sheriff <realexcel2020@gmail.com>
2023-11-04 01:02:51 +0100

    deploy app one

--- terraform/network.tf
+++ terraform/network.tf
@@ -23,11 +23,11 @@ module "vpc" {
 
 # VPC endpoint for cloudwatch logs
 
-resource "aws_vpc_endpoint" "logs" {
-  vpc_id       = module.vpc.vpc_id
-  service_name = "com.amazonaws.${var.region}.logs"
-  private_dns_enabled = true
-  security_group_ids = [ aws_security_group.ecs-sg.id ]
-  subnet_ids = module.vpc.private_subnets
-  vpc_endpoint_type = "Interface"
-}
+# resource "aws_vpc_endpoint" "logs" {
+#   vpc_id       = module.vpc.vpc_id
+#   service_name = "com.amazonaws.${var.region}.logs"
+#   private_dns_enabled = true
+#   security_group_ids = [ aws_security_group.ecs-sg.id ]
+#   subnet_ids = module.vpc.private_subnets
+#   vpc_endpoint_type = "Interface"
+# }

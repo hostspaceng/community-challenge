1dc8ddbf0c41d7e6df4a336ad716d4d20a88efe6
Sheriff <realexcel2020@gmail.com>
2023-11-05 11:14:44 +0100

    changed grafana description

--- terraform/managed-grafana.tf
+++ terraform/managed-grafana.tf
@@ -9,7 +9,7 @@ module "managed_grafana" {
 
   # Workspace
   name                      = "${var.project_name}-managed-grafana"
-  description               = "AWS Managed Grafana service for community challlenge"
+  description               = "AWS Managed Grafana service for community challlenge monitoring"
   account_access_type       = "CURRENT_ACCOUNT"
   authentication_providers  = ["AWS_SSO"]
   permission_type           = "SERVICE_MANAGED"

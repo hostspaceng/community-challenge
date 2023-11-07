6c621dd879ed2863451da4728bfa3d1d77713b96
Franklyn-dotcom <mbanugofranklyn678@gmail.com>
2023-11-07 07:54:15 -0500

    update files

--- IaC/civo.tf
+++ IaC/civo.tf
@@ -1,22 +1,23 @@
 # Query small instance size
-data "civo_size" "xsmall" {
+data "civo_size" "medium" {
     filter {
-        key = "type"
-        values = ["kubernetes"]
+        key = "name"
+        values = ["g4s.kube.medium"]
+        match_by = "re"
     }
 
 }
 
 
 # Create a firewall
-resource "civo_firewall" "my-firewall" {
-    name = "my-firewall"
+resource "civo_firewall" "my-firewall-demo" {
+    name = "my-firewall-demo"
     
 }
 
 # Create a firewall rule
 resource "civo_firewall_rule" "kubernetes_api" {
-    firewall_id = civo_firewall.my-firewall.id
+    firewall_id = civo_firewall.my-firewall-demo.id
     protocol = "tcp"
     start_port = "6443"
     end_port = "6443"
@@ -28,13 +29,13 @@ resource "civo_firewall_rule" "kubernetes_api" {
 
 
 # Create a cluster with k3s
-resource "civo_kubernetes_cluster" "k8s_demo_1" {
-    name = "k8s_demo_1"
-    applications = ""
-    firewall_id = civo_firewall.my-firewall.id
-    cluster_type = "k3s"
-    pools {
-        size = element(data.civo_size.xsmall.sizes, 0).name
-        node_count = 3
-    }
+resource "civo_kubernetes_cluster" "k8s-cluster" {
+    name       = "k8s-cluster"
+  applications = ""
+  firewall_id = civo_firewall.my-firewall-demo.id
+  num_target_nodes = 3
+  pools {
+    node_count = 3
+    size = element(data.civo_size.medium.sizes, 0).name
+  }
 }
